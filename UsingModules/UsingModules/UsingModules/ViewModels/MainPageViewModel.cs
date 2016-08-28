a9a007f57a8e40df3548c13b40ba934698063642
Davide Zordan <info@davidezordan.net>
2016-08-28 18:54:01 +0100

    Adding UsingModules sample

---
+++ UsingModules/UsingModules/UsingModules/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows.Input;
+using Prism.Commands;
+using Prism.Modularity;
+using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace UsingModules.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private readonly IModuleManager _moduleManager;
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(IModuleManager moduleManager, INavigationService navigationService)
+        {
+            _moduleManager = moduleManager;
+            _navigationService = navigationService;
+
+            LoadSampleModuleCommand = new DelegateCommand(LoadSampleModule, ()=>!IsSampleModuleRegistered)
+                .ObservesProperty(()=>IsSampleModuleRegistered);
+            NavigateToSamplePageCommand = new DelegateCommand(NavigateToSamplePage, ()=>IsSampleModuleRegistered)
+                .ObservesProperty(()=>IsSampleModuleRegistered);
+        }
+
+        private bool _isSampleModuleRegistered;
+        public bool IsSampleModuleRegistered
+        {
+            get { return _isSampleModuleRegistered; }
+            set { SetProperty(ref _isSampleModuleRegistered, value); }
+        }
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public ICommand LoadSampleModuleCommand { get; set; }
+
+        public ICommand NavigateToSamplePageCommand { get; set; }
+
+        private void NavigateToSamplePage()
+        {
+            _navigationService.NavigateAsync("SamplePage?par=test");
+        }
+
+        private void LoadSampleModule()
+        {
+            _moduleManager.LoadModule("SampleModule");
+            IsSampleModuleRegistered = true;
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
---
+++ UsingModules/UsingModules/UsingModules/Views/MainNavigationPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace UsingModules.Views
+{
+    public partial class MainNavigationPage : NavigationPage
+    {
+        public MainNavigationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
