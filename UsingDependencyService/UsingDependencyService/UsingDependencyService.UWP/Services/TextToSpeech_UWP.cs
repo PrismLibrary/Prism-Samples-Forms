3e2a93a3465b4a7506848bf101eee5e8aca6c134
Jeremy Ellis <jaellis@silverbaymgmt.com>
2017-03-16 16:06:05 -0500

    Updated to Xamarin.Forms 2.3.3.193 and added UWP project (work in progress).

---
+++ UsingDependencyService/UsingDependencyService/UsingDependencyService.UWP/Services/TextToSpeech_UWP.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UsingDependencyService.UWP.Services;
+using UsingDependencyService.Services;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(TextToSpeech_UWP))]
+
+namespace UsingDependencyService.UWP.Services
+{
+    public class TextToSpeech_UWP : ITextToSpeech
+    {
+        public void Speak(string text)
+        {
+            //TODO: Need to create UWP Speak() implementation -
+            //  probably need code from here:  https://docs.microsoft.com/en-us/uwp/api/Windows.Media.SpeechSynthesis.SpeechSynthesizer
+            throw new NotImplementedException();
+        }
+    }
+}
