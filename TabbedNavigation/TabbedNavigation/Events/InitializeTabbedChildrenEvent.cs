bfde04860a6d187ec91a78242b788b77107d4a2e
Dan Siegel <me@dansiegel.net>
2017-03-29 17:51:43 -0700

    adding sample for TabbedPage navigation

---
+++ TabbedNavigation/TabbedNavigation/Events/InitializeTabbedChildrenEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Prism.Events;
+using Prism.Navigation;
+
+namespace TabbedNavigation.Events
+{
+    public class InitializeTabbedChildrenEvent : PubSubEvent<NavigationParameters>
+    {
+
+    }
+}
---
+++ TabbedNavigation/TabbedNavigation/Views/ViewB.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TabbedNavigation.Views
+{
+    public partial class ViewB : ContentPage
+    {
+        public ViewB()
+        {
+            InitializeComponent();
+        }
+    }
+}
