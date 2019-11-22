07d69981b8039d45db6922971e9a198d5bef142a
Dan Siegel <me@dansiegel.net>
2019-11-22 15:10:49 -0800

    Adding Delegate Command sample

--- 04-Commands/src/PrismSample/ViewModels/MainPageViewModel.cs
+++ 04-Commands/src/PrismSample/ViewModels/MainPageViewModel.cs
@@ -7,5 +7,30 @@ namespace PrismSample.ViewModels
 {
     public class MainPageViewModel : BindableBase
     {
+        private bool _isActive;
+        public bool IsActive
+        {
+            get { return _isActive; }
+            set { SetProperty(ref _isActive, value); }
+        }
+
+        private string _text = "Default Text";
+        public string Text
+        {
+            get { return _text; }
+            set { SetProperty(ref _text, value); }
+        }
+
+        public DelegateCommand<string> ButtonClickCommand { get; private set; }
+
+        public MainPageViewModel()
+        {
+            ButtonClickCommand = new DelegateCommand<string>(Execute).ObservesCanExecute(() => IsActive);
+        }
+
+        private void Execute(string parameter)
+        {
+            Text = parameter;
+        }
     }
 }
