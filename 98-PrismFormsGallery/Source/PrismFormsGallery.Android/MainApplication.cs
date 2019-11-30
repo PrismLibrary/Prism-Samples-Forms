ce330569f3ac206160e0aa7c618ef7edd1dc9d43
Dan Siegel <me@dansiegel.net>
2019-11-29 16:39:58 -0800

    splash screen fixes

---
+++ 98-PrismFormsGallery/Source/PrismFormsGallery.Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+
+namespace PrismFormsGallery.Droid
+{
+    [Application(Label = "Prism Gallery",
+                 Icon = "@mipmap/icon")]
+    public class MainApplication : Application
+    {
+        public MainApplication(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+        }
+    }
+}
