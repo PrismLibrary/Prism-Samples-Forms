4ba7f1068dec6ea17e119b915cb24fc3f618be01
Michael Esteves <mike.esteves@gmail.com>
2019-12-06 21:48:57 +0200

    Example of how to use Prism's IDeviceService

--- 18-DeviceService/src/PrismSample/ViewModels/MainPageViewModel.cs
+++ 18-DeviceService/src/PrismSample/ViewModels/MainPageViewModel.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Prism.Mvvm;
+﻿using Prism.Mvvm;
+using Prism.Services;
+using Xamarin.Forms;
 
 namespace PrismSample.ViewModels
 {
     public class MainPageViewModel : BindableBase
     {
+        private readonly IDeviceService _deviceService;
+
+        public string RuntimePlatform { get; set; }
+        public string Idiom { get; set; }
+        public string XamarinRuntimePlatform { get; set; }
+        public string XamarinIdiom { get; set; }
+
+        public MainPageViewModel(IDeviceService deviceService)
+        {
+            _deviceService = deviceService;
+
+            RuntimePlatform = _deviceService.RuntimePlatform.ToString();
+            Idiom = _deviceService.Idiom.ToString();
+            
+            XamarinIdiom = Device.Idiom.ToString();
+            XamarinRuntimePlatform = Device.RuntimePlatform;
+        }
     }
 }
