1c4bec680580d49c17ba0640191712ead45f619e
Dan Siegel <me@dansiegel.net>
2020-01-10 08:11:16 -0800

    Update PageDialogService to Sample Template

---
+++ 06-PageDialogService/src/PrismSample/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Prism.Ioc;
+using PrismSample.ViewModels;
+using PrismSample.Views;
+using Xamarin.Forms;
+
+namespace PrismSample
+{
+    public partial class App
+    {
+        public App()
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            var result = await NavigationService.NavigateAsync("MainPage");
+
+            if(!result.Success)
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
