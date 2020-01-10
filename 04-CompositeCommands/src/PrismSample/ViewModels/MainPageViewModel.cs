8acc3a70bd1fee292207bfe56e500e5e04d23ce2
Dan Siegel <me@dansiegel.net>
2020-01-10 08:09:18 -0800

    Migrate Composite Commands to Sample Template

---
+++ 04-CompositeCommands/src/PrismSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace PrismSample.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        private IApplicationCommands _applicationCommand;
+        public IApplicationCommands ApplicationCommands
+        {
+            get { return _applicationCommand; }
+            set { SetProperty(ref _applicationCommand, value); }
+        }
+
+        public MainPageViewModel(IApplicationCommands applicationCommands)
+        {
+            ApplicationCommands = applicationCommands;
+        }
+    }
+}
