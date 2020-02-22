c94792b6ba02b09ca72e44c1cc549749e90f505d
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2020-02-21 22:20:26 -0600

    added inline comments about converters

--- advanced-topics/ReactiveUI/src/PrismSample/Views/NugetVersionViewCell.xaml.cs
+++ advanced-topics/ReactiveUI/src/PrismSample/Views/NugetVersionViewCell.xaml.cs
@@ -19,6 +19,7 @@ namespace PrismSample.Views
         {
             InitializeComponent();
 
+            // Note: The VersionToStringConvterer could be done inline.  Check the ReadMe.md for a link to the documentation
             this.OneWayBind(ViewModel, x => x.VersionInformation.Version, x => x.PackageVersion.Text, vmToViewConverterOverride: new VersionToStringConverter())
                 .DisposeWith(ViewCellBindings);
 
