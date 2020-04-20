65b247d3757e5588a8caddb614679edeb7c872e9
Jon Bachelor <19045+jbachelor@users.noreply.github.com>
2020-04-20 07:46:19 -0700

    Updating App.xaml.cs with Prism constructors.

--- 05-EventAggregator/src/PrismSample/App.xaml.cs
+++ 05-EventAggregator/src/PrismSample/App.xaml.cs
@@ -1,17 +1,23 @@
 ﻿using System;
+using Prism;
 using Prism.Ioc;
 using PrismSample.Navigation;
 using PrismSample.ViewModels;
 using PrismSample.Views;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace PrismSample
 {
     public partial class App
     {
-        public App()
-        {
-        }
+        /* 
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor. 
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+        public App() : this(null) { }
+        public App(IPlatformInitializer initializer) : base(initializer) { }
 
         protected override async void OnInitialized()
         {
