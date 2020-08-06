ffc73eb54b67837685bd734b2f928a132faff3e2
Dan Siegel <me@dansiegel.net>
2020-08-05 18:03:52 -0700

    use Essentials - closes #99

--- 03-PlatformSpecificServices/src/PrismSample.iOS/Services/TextToSpeech_iOS.cs
+++ 03-PlatformSpecificServices/src/PrismSample.iOS/Services/TextToSpeech_iOS.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AVFoundation;
-using Foundation;
+﻿using Xamarin.Essentials;
 using PrismSample.Services;
-using UIKit;
+using System.Threading.Tasks;
 
 namespace PrismSample.iOS.Services
 {
@@ -14,20 +8,7 @@ namespace PrismSample.iOS.Services
     {
         public Task Speak(string text)
         {
-            return new Task(() =>
-            {
-                var speechSynthesizer = new AVSpeechSynthesizer();
-
-                var speechUtterance = new AVSpeechUtterance(text)
-                {
-                    Rate = AVSpeechUtterance.MaximumSpeechRate / 2.5f,
-                    Voice = AVSpeechSynthesisVoice.FromLanguage("en-US"),
-                    Volume = 0.5f,
-                    PitchMultiplier = 1.0f
-                };
-
-                speechSynthesizer.SpeakUtterance(speechUtterance);
-            });
+            return TextToSpeech.SpeakAsync(text);
         }
     }
 }
